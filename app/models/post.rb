ec9d3bc2808f4cf91cdc9a88b30b12aa9cd2ae70
kitasho0413 <kitasho0413@gmail.com>
2022-04-18 12:10:55 +0000

    Add いいね機能

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,9 @@ class Post < ApplicationRecord
   has_one_attached :image
   
   belongs_to :player
+  has_many :favorites, dependent: :destroy
+  
+   def favorited_by?(player)
+    favorites.exists?(player_id: player.id)
+   end
 end
